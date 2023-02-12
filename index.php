e44dedde1fa183ee439dfd329f4fc9ca0d64302c
vickycuppiecake <87994731+vickycuppiecake@users.noreply.github.com>
2023-02-12 17:17:27 +0000

    Create index.php

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>My Portfolio Website</title>
+</head>
+<body>
+  <h1>Welcome to My Portfolio Website</h1>
+  <p>Here you can find information about me and my work.</p>
+</body>
+</html>
